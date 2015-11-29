618e3349df74cb4096e97d3a6c18e9fc8312f7bc
Ricardo Visual Studio <ricardof.reiter@gmail.com>
2015-11-29 14:15:50 -0200

    Criando métodos de busca no Índice

--- db-compiler/src/main/java/database/metadata/Index.java
+++ db-compiler/src/main/java/database/metadata/Index.java
@@ -3,14 +3,17 @@ package database.metadata;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Index implements Serializable {
 
 	private class IndexEntry implements Comparable<Object> {
 
 		public Object value;
-		public Set<Integer> indexes;
+		public Set<Integer> indexes = new TreeSet<Integer>();
 		
 		@Override
 		public int compareTo(Object o) {
@@ -31,7 +34,61 @@ public class Index implements Serializable {
 		if (index >= 0) {
 			return values.get(index).indexes;
 		}
-		return Collections.emptySet();
+		return new TreeSet<>();
+	}
+	
+	public Set<Integer> getIndexesEquals(final Object value) {
+		return get(value);
+	}
+	
+	public Set<Integer> getIndexesNotEquals(final Object value) {
+		int index = Collections.binarySearch(values, value);
+		Set<Integer> indexes = new TreeSet<Integer>();
+		for (int i = 0; i < values.size(); i++) {
+			if (i == index) {
+				continue;
+			}
+			indexes.addAll(values.get(i).indexes);
+		}
+		return indexes;
+	}
+	
+	public Set<Integer> getIndexesGreater(final Object value) {
+		return internalGetIndexesGreater(value, false);
+	}
+	
+	public Set<Integer> getIndexesGreaterOrEquals(final Object value) {
+		return internalGetIndexesGreater(value, true);
+	}
+	
+	public Set<Integer> getIndexesLess(final Object value) {
+		return internalGetIndexesLess(value, false);
+	}
+	
+	public Set<Integer> getIndexesLessOrEquals(final Object value) {
+		return internalGetIndexesLess(value, true);
+	}
+
+	private Set<Integer> internalGetIndexesLess(final Object value, boolean inclusive) {
+		Set<Integer> indexes = new TreeSet<Integer>();
+		
+		int index = Collections.binarySearch(values, value);
+		int finalIndex = (index >= 0) ? ((inclusive) ? index + 1 : index) : values.size();
+		for (int i = 0; i < finalIndex; i++) {
+			indexes.addAll(values.get(i).indexes);
+		}
+		return indexes;
+	}
+	
+	private Set<Integer> internalGetIndexesGreater(final Object value, boolean inclusive) {
+		Set<Integer> indexes = new TreeSet<Integer>();
+		
+		int index = Collections.binarySearch(values, value);
+		int initialIndex = (index >= 0) ? ((inclusive) ? index : index + 1) : 0;
+		for (int i = initialIndex; i < values.size(); i++) {
+			indexes.addAll(values.get(i).indexes);
+		}
+		return indexes;
 	}
 
 	public void put(final Object key, final Integer index) {
